9ba19d06535b09a9ccd441a2db66647150ddaa3e
emmanuelmusik <emmanuelmusik7@gmail.com>
2026-09-04 14:19:04 +0200

    Add files via upload

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -122,7 +122,7 @@ export function AuthProvider({ children }) {
     isAuthenticated: !!session,
     signUp: (email, password) => supabase.auth.signUp({ email, password }),
     signIn: (email, password) => supabase.auth.signInWithPassword({ email, password }),
-    signInWithGoogle: signInWithGoogle,
+    signInWithGoogle: () => supabase.auth.signInWithOAuth({ provider: 'google' }),
     signInWithApple: signInWithApple,
     signOut: () => supabase.auth.signOut(),
   };
